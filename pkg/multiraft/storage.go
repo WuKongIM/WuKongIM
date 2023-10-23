25cd980a4df72698978eb7b8d898748871745cfa
tangtaoit <tangtaoit@githubim.com>
2023-10-23 11:01:50 +0800

    feat: multi raft

--- pkg/multiraft/storage.go
+++ pkg/multiraft/storage.go
@@ -5,101 +5,116 @@ import (
 	"fmt"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/WuKongIM/WuKongIM/pkg/multiraft/wal"
 	"github.com/WuKongIM/WuKongIM/pkg/wklog"
-	bolt "go.etcd.io/bbolt"
+	"go.etcd.io/raft/v3"
 	pb "go.etcd.io/raft/v3/raftpb"
 	"go.uber.org/zap"
 )
 
-type SateStorage interface {
-	InitialState() (hardState pb.HardState, confState pb.ConfState, err error)
-	SetHardState(st pb.HardState) error
-	HardState() (pb.HardState, error)
-	SetConfState(confState pb.ConfState) error
-	SetApplied(applied uint64) error
-	Applied() (uint64, error)
-}
+// type SateStorage interface {
+// 	InitialState() (hardState pb.HardState, confState pb.ConfState, err error)
+// 	SetHardState(st pb.HardState) error
+// 	HardState() (pb.HardState, error)
+// 	SetConfState(confState pb.ConfState) error
+// 	SetApplied(applied uint64) error
+// 	Applied() (uint64, error)
+// }
 
-type LogStorage struct {
-	walStore    *WALStorage
-	id          uint64
-	sateStorage SateStorage
-}
+// type LogStorage struct {
+// 	walStore    *WALStorage
+// 	replicaID   uint32
+// 	raftStorage ReplicaRaftStorage
+// 	peers       []Peer
+// }
 
-func NewLogStorage(id uint64, walStore *WALStorage, sateStorage SateStorage) *LogStorage {
-	return &LogStorage{
-		walStore:    walStore,
-		sateStorage: sateStorage,
-	}
-}
+// func NewLogStorage(replicaID uint32, walStore *WALStorage, raftStorage ReplicaRaftStorage, peers []Peer) *LogStorage {
+// 	return &LogStorage{
+// 		peers:       peers,
+// 		replicaID:   replicaID,
+// 		walStore:    walStore,
+// 		raftStorage: raftStorage,
+// 	}
+// }
 
-func (l *LogStorage) InitialState() (hardState pb.HardState, confState pb.ConfState, err error) {
+// func (l *LogStorage) InitialState() (hardState pb.HardState, confState pb.ConfState, err error) {
 
-	return l.sateStorage.InitialState()
-}
+// 	confState, err = l.raftStorage.GetConfState(l.replicaID)
+// 	if err != nil {
+// 		return
+// 	}
+// 	if len(confState.Voters) == 0 {
+// 		peerIDs := make([]uint64, 0, len(l.peers))
+// 		for _, peer := range l.peers {
+// 			peerIDs = append(peerIDs, peer.ID)
+// 		}
+// 		confState.Voters = peerIDs
+// 	}
+// 	hardState, err = l.raftStorage.GetHardState(l.replicaID)
+// 	if err != nil {
+// 		return
+// 	}
+// 	return
+// }
 
-func (l *LogStorage) SetHardState(st pb.HardState) error {
-	return l.sateStorage.SetHardState(st)
-}
+// func (l *LogStorage) SetHardState(st pb.HardState) error {
+// 	return l.raftStorage.SetHardState(l.replicaID, st)
+// }
 
-func (l *LogStorage) HardState() (pb.HardState, error) {
-	return l.sateStorage.HardState()
-}
+// func (l *LogStorage) HardState() (pb.HardState, error) {
+// 	return l.raftStorage.GetHardState(l.replicaID)
+// }
 
-func (l *LogStorage) SetConfState(confState pb.ConfState) error {
-	return l.sateStorage.SetConfState(confState)
-}
+// func (l *LogStorage) SetConfState(confState pb.ConfState) error {
+// 	return l.raftStorage.SetConfState(l.replicaID, confState)
+// }
 
-func (l *LogStorage) SetApplied(applied uint64) error {
-	return l.sateStorage.SetApplied(applied)
-}
+// func (l *LogStorage) SetApplied(applied uint64) error {
+// 	return l.raftStorage.SetApplied(l.replicaID, applied)
+// }
 
-func (l *LogStorage) Applied() (uint64, error) {
-	return l.sateStorage.Applied()
-}
+// func (l *LogStorage) Applied() (uint64, error) {
+// 	return l.raftStorage.GetApplied(l.replicaID)
+// }
 
-func (l *LogStorage) Entries(lo, hi, maxSize uint64) ([]pb.Entry, error) {
-	return l.walStore.Entries(lo, hi, maxSize)
-}
+// func (l *LogStorage) Entries(lo, hi, maxSize uint64) ([]pb.Entry, error) {
+// 	return l.walStore.Entries(lo, hi, maxSize)
+// }
 
-func (l *LogStorage) Append(entries []pb.Entry) error {
-	return l.walStore.Append(entries)
-}
+// func (l *LogStorage) Append(entries []pb.Entry) error {
+// 	return l.walStore.Append(entries)
+// }
 
-func (l *LogStorage) Term(i uint64) (uint64, error) {
-	return l.walStore.Term(i)
-}
+// func (l *LogStorage) Term(i uint64) (uint64, error) {
+// 	return l.walStore.Term(i)
+// }
 
-func (l *LogStorage) LastIndex() (uint64, error) {
-	return l.walStore.LastIndex()
-}
+// func (l *LogStorage) LastIndex() (uint64, error) {
+// 	return l.walStore.LastIndex()
+// }
 
-func (l *LogStorage) FirstIndex() (uint64, error) {
-	return l.walStore.FirstIndex()
-}
+// func (l *LogStorage) FirstIndex() (uint64, error) {
+// 	return l.walStore.FirstIndex()
+// }
 
-func (l *LogStorage) Snapshot() (pb.Snapshot, error) {
-	return l.walStore.Snapshot()
-}
+// func (l *LogStorage) Snapshot() (pb.Snapshot, error) {
+// 	return l.walStore.Snapshot()
+// }
 
-func (l *LogStorage) ApplySnapshot(snap pb.Snapshot) error {
-	return l.walStore.ApplySnapshot(snap)
-}
+// func (l *LogStorage) ApplySnapshot(snap pb.Snapshot) error {
+// 	return l.walStore.ApplySnapshot(snap)
+// }
 
 type WALStorage struct {
 	sync.Mutex
 	walLog *wal.Log
 	wklog.Log
-	metaDB *bolt.DB
 
 	hardStateKey      []byte
 	confStateKey      []byte
 	committedIndexKey []byte
 	appliedKey        []byte
-	peersKey          []byte
 	walPath           string
 	metaPath          string
 }
@@ -111,7 +126,6 @@ func NewWALStorage(walPath string) *WALStorage {
 		committedIndexKey: []byte("committedIndex"),
 		confStateKey:      []byte("confState"),
 		appliedKey:        []byte("appliedKey"),
-		peersKey:          []byte("peersKey"),
 		walPath:           walPath,
 	}
 
@@ -130,34 +144,6 @@ func (w *WALStorage) Open() error {
 	}
 	w.walLog = lg
 
-	w.metaDB, err = bolt.Open(w.metaPath, 0755, &bolt.Options{Timeout: 10 * time.Second})
-	if err != nil {
-		return err
-	}
-
-	err = w.metaDB.Batch(func(t *bolt.Tx) error {
-		_, err := t.CreateBucketIfNotExists(w.hardStateKey)
-		if err != nil {
-			return err
-		}
-		_, err = t.CreateBucketIfNotExists(w.committedIndexKey)
-		if err != nil {
-			return err
-		}
-		_, err = t.CreateBucketIfNotExists(w.confStateKey)
-		if err != nil {
-			return err
-		}
-		_, err = t.CreateBucketIfNotExists(w.appliedKey)
-		if err != nil {
-			return err
-		}
-		_, err = t.CreateBucketIfNotExists(w.peersKey)
-		return err
-	})
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
@@ -166,10 +152,7 @@ func (w *WALStorage) Close() error {
 	if err != nil {
 		w.Warn("close wal log error", zap.Error(err))
 	}
-	err = w.metaDB.Close()
-	if err != nil {
-		w.Warn("close meta db error", zap.Error(err))
-	}
+
 	return nil
 }
 
@@ -286,4 +269,67 @@ func limitSize(ents []pb.Entry, maxSize uint64) []pb.Entry {
 		}
 	}
 	return ents[:limit]
+}
+
+type RaftStorage interface {
+	raft.Storage
+	Append(entries []pb.Entry) error
+	SetHardState(hardState pb.HardState) error
+	SetConfState(confState pb.ConfState) error
+}
+
+type MemoryRaftStorage struct {
+	memoryStorage *raft.MemoryStorage
+	confsate      *pb.ConfState
+}
+
+func NewMemoryRaftStorage() *MemoryRaftStorage {
+	return &MemoryRaftStorage{
+		memoryStorage: raft.NewMemoryStorage(),
+	}
+}
+
+func (m *MemoryRaftStorage) InitialState() (pb.HardState, pb.ConfState, error) {
+	return m.memoryStorage.InitialState()
+}
+
+func (m *MemoryRaftStorage) Entries(lo, hi, maxSize uint64) ([]pb.Entry, error) {
+	return m.memoryStorage.Entries(lo, hi, maxSize)
+}
+
+func (m *MemoryRaftStorage) Term(i uint64) (uint64, error) {
+	return m.memoryStorage.Term(i)
+}
+
+func (m *MemoryRaftStorage) LastIndex() (uint64, error) {
+	return m.memoryStorage.LastIndex()
+}
+
+func (m *MemoryRaftStorage) FirstIndex() (uint64, error) {
+	return m.memoryStorage.FirstIndex()
+}
+
+func (m *MemoryRaftStorage) Snapshot() (pb.Snapshot, error) {
+	return m.memoryStorage.Snapshot()
+}
+
+func (m *MemoryRaftStorage) ApplySnapshot(snap pb.Snapshot) error {
+	return m.memoryStorage.ApplySnapshot(snap)
+}
+
+func (m *MemoryRaftStorage) SetHardState(hardState pb.HardState) error {
+	return m.memoryStorage.SetHardState(hardState)
+}
+
+func (m *MemoryRaftStorage) SetConfState(confState pb.ConfState) error {
+	m.confsate = &confState
+	return nil
+}
+func (m *MemoryRaftStorage) GetConfState() pb.ConfState {
+	return *m.confsate
+}
+
+func (m *MemoryRaftStorage) Append(entries []pb.Entry) error {
+	fmt.Println("Append...........", entries[len(entries)-1].Index)
+	return m.memoryStorage.Append(entries)
 }
