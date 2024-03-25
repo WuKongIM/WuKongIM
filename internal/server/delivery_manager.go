9117332ceee1f1c52145a5e6de4e53406fb9eb83
tangtaoit <tangtaoit@githubim.com>
2024-03-25 22:24:53 +0800

    fix: ants.NewPool panic log print

--- internal/server/delivery_manager.go
+++ internal/server/delivery_manager.go
@@ -23,7 +23,7 @@ func NewDeliveryManager(s *Server) *DeliveryManager {
 	options := ants.Options{ExpiryDuration: 10 * time.Second, Nonblocking: false}
 	deliveryMsgPool, err := ants.NewPool(s.opts.DeliveryMsgPoolSize, ants.WithOptions(options), ants.WithPanicHandler(func(err interface{}) {
 		stack := debug.Stack()
-		s.Error("消息投递panic->", zap.Error(err.(error)), zap.String("stack", string(stack)))
+		s.Error("消息投递panic", zap.Error(err.(error)), zap.String("stack", string(stack)))
 	}))
 	if err != nil {
 		panic(err)
