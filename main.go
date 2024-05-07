c8cb2911f4e426a81c2f7f80276bbaaf2f0892ed
tangtaoit <tangtaoit@githubim.com>
2024-05-07 11:36:48 +0800

    fix: remove syscall.Dup2

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"embed"
-	"log"
-	"os"
-	"syscall"
 
 	"github.com/WuKongIM/WuKongIM/cmd"
 	"github.com/WuKongIM/WuKongIM/version"
@@ -31,13 +28,13 @@ func main() {
 	version.WebFs = webFS
 	version.DemoFs = demoFS
 
-	logFile, err := os.OpenFile("./fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0660)
-	if err != nil {
-		log.Println("服务启动出错", "打开异常日志文件失败", err)
-		return
-	}
-	// 将进程标准出错重定向至文件，进程崩溃时运行时将向该文件记录协程调用栈信息
-	syscall.Dup2(int(logFile.Fd()), int(os.Stderr.Fd()))
+	// logFile, err := os.OpenFile("./fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0660)
+	// if err != nil {
+	// 	log.Println("服务启动出错", "打开异常日志文件失败", err)
+	// 	return
+	// }
+	// // 将进程标准出错重定向至文件，进程崩溃时运行时将向该文件记录协程调用栈信息
+	// syscall.Dup2(int(logFile.Fd()), int(os.Stderr.Fd()))
 
 	cmd.Execute()
 }
