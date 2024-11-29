a66e37bc1b6ec7359746966f02dcd5b2a94dd44a
tangtaoit <tangtaoit@githubim.com>
2024-11-29 23:49:06 +0800

    fix: incorrect make receiver tag

--- pkg/cluster/icluster/cluster.go
+++ pkg/cluster/icluster/cluster.go
@@ -50,7 +50,7 @@ type Cluster interface {
 	// SlotLeaderNodeInfo 获取槽的节点信息
 	SlotLeaderNodeInfo(slotId uint32) (nodeInfo *pb.Node, err error)
 
-	// LoadOnlyChannelClusterConfig 加载频道配置，仅仅只加载配置，不创建和激活频道
+	// LoadOnlyChannelClusterConfig 加载频道配置，仅仅只加载配置，
 	LoadOnlyChannelClusterConfig(channelId string, channelType uint8) (wkdb.ChannelClusterConfig, error)
 
 	// 领导者Id
