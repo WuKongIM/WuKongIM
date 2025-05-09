27938da5846aae458394ea42c2264626e0568748
tt <tangtaoit@githubim.com>
2025-05-09 21:05:28 +0800

    fix: add check conn valid

--- internal/user/event/pool.go
+++ internal/user/event/pool.go
@@ -7,7 +7,6 @@ import (
 	"github.com/WuKongIM/WuKongIM/internal/common"
 	"github.com/WuKongIM/WuKongIM/internal/eventbus"
 	"github.com/WuKongIM/WuKongIM/internal/options"
-	"github.com/WuKongIM/WuKongIM/internal/service"
 	"github.com/WuKongIM/WuKongIM/pkg/wklog"
 	"github.com/WuKongIM/WuKongIM/pkg/wknet"
 	wkproto "github.com/WuKongIM/WuKongIMGoProto"
