3c21cb9f49b541151e1596809119efd7db2b41ad
tt <tangtaoit@githubim.com>
2026-06-13 00:40:14 +0800

    test(channelappend): cover advance() effect-buffer reuse for bleed and races

--- internalv2/runtime/channelappend/writer_advance_test.go
+++ internalv2/runtime/channelappend/writer_advance_test.go
@@ -49,3 +49,45 @@ func TestWriterAdvanceCompletesWithinDeadline(t *testing.T) {
 		t.Fatalf("wait error = %v", err)
 	}
 }
+
+func TestAdvancePostCommitReusesEffectWithoutBleed(t *testing.T) {
+	group := New(Options{
+		LocalNodeID:                1,
+		AuthorityShardCount:        1,
+		EffectPoolSize:             4,
+		AdmissionCapacityPerShard:  4096,
+		Appender:                   &orderedAppender{},
+		MessageID:                  newBenchmarkMessageIDs(1),
+		ConversationActiveAdmitter: benchmarkNoopActiveAdmitter{},
+	})
+	if err := group.Start(context.Background()); err != nil {
+		t.Fatalf("Start() error = %v", err)
+	}
+	t.Cleanup(func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := group.Stop(ctx); err != nil {
+			t.Fatalf("Stop() error = %v", err)
+		}
+	})
+
+	target := benchmarkAuthorityTarget("reuse-1")
+	const batches = 200
+	futures := make([]*Future, batches)
+	for i := 0; i < batches; i++ {
+		f, err := group.SubmitLocal(context.Background(), target, []SendBatchItem{benchmarkSendItem("reuse-1")})
+		if err != nil {
+			t.Fatalf("submit %d error = %v", i, err)
+		}
+		futures[i] = f
+	}
+	for i, f := range futures {
+		res, err := f.Wait(context.Background())
+		if err != nil {
+			t.Fatalf("wait %d error = %v", i, err)
+		}
+		if len(res) != 1 || res[0].Err != nil || res[0].Result.Reason != ReasonSuccess {
+			t.Fatalf("batch %d result = %#v, want one successful result", i, res)
+		}
+	}
+}
